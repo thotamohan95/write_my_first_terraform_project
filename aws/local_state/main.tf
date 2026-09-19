a2d6fcd777af13cafdbcbd745d1ecc9ab378b19d
thotamohan95 <thotamohan95@gmail.com>
2026-09-19 15:16:22 +0530

    Update AWS provider version and instance configuration

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,24 +1,32 @@
+# Terraform Settings Block
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.67"
     }
   }
 
   required_version = ">= 1.2.0"
 }
-
+# Provider Block
 provider "aws" {
   region  = "us-east-1"
 }
-
+# Resource Block
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = "ami-0006118602dfc1c09"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform_Demo"
   }
 }
 
+
+# Commands to run the Terraform scripts.
+# terraform init
+# terraform validate
+# terraform plan
+# terraform apply
+# terraform destroy
